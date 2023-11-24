a7926a6d30c406f8cf004b1b388406410cb7419d
muftialroaz <azamalfarokhul@gmail.com>
2023-11-24 10:57:18 +0700

    app.py is a json module

--- main.py
+++ main.py
@@ -21,10 +21,10 @@ def index():
         rekam_medis['cosine'] = (nama_pasien['cosine'] + diagnosis['cosine'] + nama_dokter['cosine']) / 3
         rekam_medis = rekam_medis.sort_values(by='cosine', ascending=False)
 
-        diagnosis_nama = rekam_medis.loc[rekam_medis['cosine'] > 0, 'diagnosis_nama']
-        pasien_nama = rekam_medis.loc[rekam_medis['cosine'] > 0, 'pasien_nama']
-        dokter_nama = rekam_medis.loc[rekam_medis['cosine'] > 0, 'dokter_nama']
-        cosine = rekam_medis.loc[rekam_medis['cosine'] > 0, 'cosine']
+        diagnosis_nama = rekam_medis.loc[rekam_medis['cosine'] > 0, 'diagnosis_nama'].tolist()
+        pasien_nama = rekam_medis.loc[rekam_medis['cosine'] > 0, 'pasien_nama'].tolist()
+        dokter_nama = rekam_medis.loc[rekam_medis['cosine'] > 0, 'dokter_nama'].tolist()
+        cosine = rekam_medis.loc[rekam_medis['cosine'] > 0, 'cosine'].tolist()
 
         # return render_template('index.html', input_data=input_data, nama_pasien=nama_pasien, diagnosis=diagnosis, nama_dokter=nama_dokter)
         return render_template('index.jinja', input_data=input_data, nama_pasien=pasien_nama, diagnosis=diagnosis_nama, nama_dokter=dokter_nama, cosine=cosine)
